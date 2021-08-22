3fe918a7699b710f3f30d6fceae5d5068a36e1c0
Bludoid <79199839+Bludoid@users.noreply.github.com>
2021-08-23 00:46:44 +0200

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -14,7 +14,7 @@ class Card {
 
 class Cards {
     constructor() {
-        this.myRanks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
+        this.myRanks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
         this.mySuits = ["&hearts;", "&diams;", "&spades;", "&clubs;"];
         this.deckie = [];
     }
