1d0a06a4ff23813d04bd3fa3f16d2ab09639e269
Bludoid <79199839+Bludoid@users.noreply.github.com>
2021-08-22 23:05:07 +0200

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,38 +1,81 @@
+"use script";
+
 class Card {
     constructor(rank, suit) {
-    this.rank = rank;
-    this.suit = suit;
+        this.rank = rank;
+        this.suit = suit;
     }
   
-    shuffleDeck() {
-        // shuffle deck
+    displayCard() {
+        return String(this.rank + this.suit);
+    }
+}
+
+
+class Cards {
+    constructor() {
+        this.myRanks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
+        this.mySuits = ["&hearts;", "&diams;", "&spades;", "&clubs;"];
+        this.deckie = [];
+    }
+    
+    createDeck() {
+        for (let mySuit of this.mySuits) {
+            for (let myRank of this.myRanks) {
+                let myCard = new Card(myRank, mySuit);
+                this.deckie.push(myCard);
+            }
+        }
     }
     
     displayDeck() {
-        // display deck
-        //let myBoard = document.getElementbyId("game");
-        //myBoard.innerHTML += this.rank + this.suit;
-        //myBoard.innerHTML += "<br>";
-        console.log(this.rank + this.suit);
-    }
-}
-
-
-
-function createDeck() {
-    let myRanks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-    let mySuits = ["h", "d", "s", "c"];
-    var myDeck = [];
-    for (let mySuit of mySuits) {
-        for (let myRank of myRanks) {
-            let myCard = new Card(myRank, mySuit);
-            myDeck.push(myCard);
+        let myDiv = getMe("deckDiv");
+        myDiv.innerHTML = "";
+        let i=1;
+        for (let myCard of deck.deckie) {
+            let cardSpan = document.createElement("SPAN");
+            cardSpan.innerHTML = (myCard.displayCard());
+            if (myCard.suit == "&hearts;" || myCard.suit == "&diams;") {
+                cardSpan.className = "redCard";
+            }
+            myDiv.appendChild(cardSpan);
+            if (i%13===0) {
+                let breaky = document.createElement("BR");
+                myDiv.appendChild(breaky);
+            }
+            i++;
         }
     }
-    return myDeck;
+    
+    shuffleDeck() {
+        for (let i = 51; i>0; i--) {
+            let j = Math.floor(Math.random()*(i+1));
+            [this.deckie[i], this.deckie[j]] = [this.deckie[j], this.deckie[i]];
+        }
+    }
 }
 
-let myDeckCards = createDeck();
-for (let oneCard of myDeckCards) {
-    oneCard.displayDeck();
+const deck = new Cards();
+
+
+function getMe(myID){
+    return document.getElementById(myID);
 }
+
+function createDeckButton() {
+    deck.createDeck();
+    deck.displayDeck();
+    getMe("showButton").style = "display: none";
+    getMe("shuffleButton").style = "visibility: visible";
+}
+
+function shuffleDeckButton() {
+    deck.shuffleDeck();
+    deck.displayDeck();
+}
+
+
+
+
+
+
